958a24ee22eede59c7511d2f17850fd32bf760d6
Jan Harms <jan.gerrit.harms@gmail.com>
2016-02-17 09:39:07 +0100

    Tried to add fixed fragment size

--- rosbridge_library/src/rosbridge_library/capabilities/fragmentation.py
+++ rosbridge_library/src/rosbridge_library/capabilities/fragmentation.py
@@ -37,7 +37,7 @@ import math
 class Fragmentation(Capability):
     """ The Fragmentation capability doesn't define any incoming operation
     handlers, but provides methods to fragment outgoing messages """
-    
+
     fragmentation_seed = 0
 
     def __init__(self, protocol):
@@ -47,13 +47,13 @@ class Fragmentation(Capability):
     def fragment(self, message, fragment_size, mid=None):
         """ Serializes the provided message, then splits the serialized
         message according to fragment_size, then sends the fragments.
-        
+
         If the size of the message is less than the fragment size, then
         the original message is returned rather than a single fragment
-        
+
         Since fragmentation is typically only used for very large messages,
         this method returns a generator for fragments rather than a list
-        
+
         Keyword Arguments
         message       -- the message dict object to be fragmented
         fragment_size -- the max size for the fragments
@@ -66,12 +66,12 @@ class Fragmentation(Capability):
         if mid is None:
             mid = self.fragmentation_seed
             self.fragmentation_seed = self.fragmentation_seed + 1
-            
+
         serialized = self.protocol.serialize(message, mid)
-        
+
         if serialized is None:
             return []
-        
+
         message_length = len(serialized)
         if message_length <= fragment_size:
             return [message]
@@ -84,23 +84,50 @@ class Fragmentation(Capability):
         self.protocol.log("info", log_msg)
 
         return self._fragment_generator(serialized, fragment_size, mid)
-    
+
     def _fragment_generator(self, msg, size, mid):
         """ Returns a generator of fragment messages """
-        total = ((len(msg)-1) / size) + 1
-        n = 0
-        for i in range(0, len(msg), size):
-            fragment = msg[i:i+size]
-            yield self._create_fragment(fragment, n, total, mid)
-            n = n + 1
-    
-    def _create_fragment(self, fragment, num, total, mid):
-        """ Given a string fragment of the original message, creates
-        the appropriate fragment message """
-        return {
+        msg = {
             "op": "fragment",
             "id": mid,
-            "data": fragment,
-            "num": num,
-            "total": total
+            "data": "",
+            "num": 0,
+            "total": 0,
+            "fill": ""
         }
+        header_size = len(str(msg))
+        total = 0
+        msg_length = 0
+        try:
+            (total, msg_length) = self._determine_n_fragments(len(msg), size, header_size)
+        except RuntimeError:
+            self.protocol.log("error", "Header size is to large for message size")
+            return []
+        msg['total'] = total
+        n = 0
+        for i in range(0, total):
+            fragment = msg[i:i+msg_length]
+            msg['data'] = fragment
+            msg['num'] = n
+            fill_length = size - len(str(fragment))
+            msg['fill'] = "".join(["0" for i in xrange(fill_length)])
+            n = n + 1
+
+    def _determine_n_fragments(self, msg_len, size, header_size):
+        """ Recursive function to determin the number and size of fragments """
+        A = [msg_len + header_size]
+        total = 1
+
+        while A[-1]/total > size:
+            msg_len += header_size
+            temp = total +1
+            temp_log = math.floor(math.log(temp)/math.log(10))
+            total_log = math.floor(math.log(total)/math.log(10))
+            if temp_log > total_log:
+                msg_len += temp
+                header_size += 2
+            total = temp
+            A.append(msg_len)
+
+        print (msg_len, header_size)
+        return (msg_len/total, total)
